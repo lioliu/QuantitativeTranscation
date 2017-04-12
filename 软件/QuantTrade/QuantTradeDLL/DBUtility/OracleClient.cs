5fcd732f1ee43a02803e10aad374e846dfb1836f
lioliu <744596028@qq.com>
2017-04-12 22:25:42 +0800

    add oracle  connect code ;

--- 软件/QuantTrade/QuantTradeDLL/DBUtility/OracleClient.cs
+++ 软件/QuantTrade/QuantTradeDLL/DBUtility/OracleClient.cs
@@ -3,10 +3,239 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Oracle.DataAccess.Client;
+using System.Data;
 namespace QuantTradeDLL.DBUtility
 {
-    class OracleClient
+    public class OracleClient
     {
+        private static string PowerConnect = "Data Source=( DESCRIPTION = ( ADDRESS = ( PROTOCOL = TCP ) ( HOST = 10.18.3.229 ) ( PORT = 1521 ) ) ( CONNECT_DATA = ( SERVICE_NAME=ORCL ) ) );user id=lioliu;password=647094;";
+
+        #region Execute sql
+        /// <summary>
+        /// Execute SQL statement 
+        /// </summary>
+        /// <param name="sql">Statement to be executed</param>
+        /// <returns> affected rows</returns>
+        public static int ExecuteSQL(string sql)
+        {
+            int count = 0;
+            OracleConnection con = new OracleConnection(PowerConnect);
+            try
+            {
+                con.Open();
+                OracleCommand cmd = new OracleCommand(sql, con);
+                count = cmd.ExecuteNonQuery();
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            finally
+            {
+                con.Close();
+                con.Dispose();
+            }
+            return count;
+        }
+        /// <summary>
+        /// Execute SQL statement
+        /// </summary>
+        /// <param name="sqlList">the list of Statements to be executed</param>
+        /// <returns> affected rows</returns>
+        public static int ExecuteSQL(List<string> sqlList)
+        {
+            int count = 0;
+            OracleConnection con = new OracleConnection(PowerConnect);
+            con.Open();
+            OracleCommand cmd = new OracleCommand()
+            {
+                Connection = con
+            };
+            OracleTransaction transaction = con.BeginTransaction();
+            cmd.Transaction = transaction;
+            try
+            {
+                foreach (string sql in sqlList)
+                {
+                    cmd.CommandText = sql;
+                    count += cmd.ExecuteNonQuery();
+                }
+                transaction.Commit();
+            }
+            catch (Exception e)
+            {
+                transaction.Rollback();
+                throw e;
+            }
+            finally
+            {
+                con.Close();
+                con.Dispose();
+                transaction.Dispose();
+                cmd.Dispose();
+            }
+            return count;
+        }
+        #endregion
+# region GetData
+        /// <summary>
+        /// get the data by execute the sql
+        /// </summary>
+        /// <param name="sql">query statement</param>
+        /// <returns>result data</returns>
+        public static DataSet GetData(string sql)
+        {
+            DataSet dataSet = new DataSet();
+            DataTable dataTable = new DataTable();
+            OracleDataAdapter oda = null;
+            try
+            {
+                oda = new OracleDataAdapter(sql, PowerConnect);
+                oda.Fill(dataTable);
+                dataSet.Tables.Add(dataTable);
+                oda.Dispose();
+
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            finally
+            {
+                oda.Dispose();
+                dataTable.Dispose();
+            }
+            return dataSet;
+        }
+
+        /// <summary>
+        /// Execute SQL statement 
+        /// </summary>
+        /// <param name="sqlList">the list of the statements to be execute</param>
+        /// <returns>result data</returns>
+        public static DataSet GetData(List<string> sqlList)
+        {
+            DataSet dataSet = new DataSet();
+            DataTable dataTable = new DataTable();
+            OracleDataAdapter oda = null;
+            OracleConnection con = new OracleConnection(PowerConnect);
+            try
+            {
+                con.Open();
+                OracleCommand cmd = new OracleCommand()
+                {
+                    Connection = con
+                };
+                foreach (string sql in sqlList)
+                {
+                    cmd.CommandText = sql;
+                    oda = new OracleDataAdapter(cmd);
+                    oda.Fill(dataTable);
+                    dataSet.Tables.Add(dataTable);
+                    dataTable = new DataTable();
+                }
+            }
+            catch (Exception e)
+            {
+              
+                throw e;
+            }
+            finally
+            {
+                con.Dispose();
+                oda.Dispose();
+                dataTable.Dispose();
+            }
+
+            return dataSet;
+        }
+
+        #endregion
+        /// <summary>
+        /// 执行存储过程返回数据存储在OracleParameter中
+        /// </summary>
+        /// <param name="ProName">存储过程名称</param>
+        /// <param name="parm">输入参数</param>
+        /// <returns>OracleParameter集</returns>
+        public static OracleParameter[] ProceureToParameter(string ProName, OracleParameter[] parm)
+        {
+            DataTable dt = new DataTable();
+            OracleConnection con = new OracleConnection(PowerConnect);
+            OracleCommand cmd = new OracleCommand();
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.CommandText = ProName;
+            DataSet ds = new DataSet();
+            try
+            {
+                con.Open();
+                cmd.Connection = con;
+                for (int i = 0; i < parm.Length; i++)
+                {
+                    cmd.Parameters.Add(parm[i]);
+                }
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            finally
+            {
+                if (con.State == ConnectionState.Open)
+                    con.Close();
+                con.Dispose();
+                cmd.Parameters.Clear();
+                cmd.Dispose();
+            }
+            return parm;
+        }
+        /// <summary>
+        /// 执行存储过程返回数据放在DataSet中
+        /// </summary>
+        /// <param name="ProName">存储过程名称</param>
+        /// <param name="parm">输入参数</param>
+        /// <returns>表格集</returns>
+        public static DataSet ProceureToTable(string ProName, OracleParameter[] parm)
+        {
+            DataTable dt = new DataTable();
+            OracleDataAdapter dr = new OracleDataAdapter();
+            OracleConnection con = new OracleConnection(PowerConnect);
+            OracleCommand cmd = new OracleCommand()
+            {
+                CommandType = CommandType.StoredProcedure,
+                CommandText = ProName
+            };
+            DataSet ds = new DataSet();
+
+            try
+            {
+                con.Open();
+                cmd.Connection = con;
+                for (int i = 0; i < parm.Length; i++)
+                {
+                    cmd.Parameters.Add(parm[i]);
+                }
+                
+                dr.SelectCommand = cmd;
+                //cmd.ExecuteNonQuery();
+                dr.Fill(ds);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            finally
+            {
+                if (con.State == ConnectionState.Open)
+                    con.Close();
+                con.Dispose();
+                cmd.Parameters.Clear();
+                cmd.Dispose();
+            }
+            return ds;
+        }
+
+
     }
 }
---
+++ 软件/QuantTrade/QuantTradeDLLTests/DBUtility/OracleClientTests.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using QuantTradeDLL.DBUtility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using Oracle.DataAccess.Client;
+
+namespace QuantTradeDLL.DBUtility.Tests
+{
+    [TestClass()]
+    public class OracleClientTests
+    {
+        [TestMethod()]
+        public void ExecuteSQLTest()
+        {
+            Assert.AreEqual(OracleClient.ExecuteSQL("SELECT SYSDATE FROM DUAL"), -1);
+
+        }
+
+        [TestMethod()]
+        public void ExecuteSQLTest1()
+        {
+            List<string> sqlList = new List<string>
+            {
+                "SELECT SYSDATE FROM DUAL"
+            };
+            Assert.AreEqual(OracleClient.ExecuteSQL(sqlList), -1);
+        }
+
+        [TestMethod()]
+        public void GetDataTest()
+        {
+            StringAssert.Contains("2017-04-05", OracleClient.GetData("SELECT '2017-04-05' FROM DUAL").Tables[0].Rows[0][0].ToString());
+        }
+
+        [TestMethod()]
+        public void GetDataTest1()
+        {
+            List<string> sqlList = new List<string>
+            {
+                "SELECT '2017-04-05' FROM DUAL",
+                "SELECT '2017-04-06' FROM DUAL"
+            };
+            DataSet dataSet = OracleClient.GetData(sqlList);
+            StringAssert.Contains("2017-04-05", dataSet.Tables[0].Rows[0][0].ToString());
+            StringAssert.Contains("2017-04-06", dataSet.Tables[1].Rows[0][0].ToString());
+        }
+
+        [TestMethod()]
+        public void ProceureToParameterTest()
+        {
+            OracleParameter[] parameter = new OracleParameter[1];
+            parameter[0] = new OracleParameter("PARAM1", OracleDbType.Varchar2, 200);
+            parameter[0].Direction = ParameterDirection.Output;
+            OracleClient.ProceureToParameter("LIOLIU.TESTSET.TEST", parameter);
+            StringAssert.Contains("success", parameter[0].Value.ToString());
+        }
+
+        [TestMethod()]
+        public void ProceureToTableTest()
+        {
+            OracleParameter[] parameter = new OracleParameter[1];
+            parameter[0] = new OracleParameter("PARAM1", OracleDbType.RefCursor)    
+            {
+                Direction = ParameterDirection.Output
+            };
+            DataSet dataSet = OracleClient.ProceureToTable("LIOLIU.TESTSET.TEST2", parameter);
+            StringAssert.Contains("success", dataSet.Tables[0].Rows[0][0].ToString());
+        }
+    }
+}
