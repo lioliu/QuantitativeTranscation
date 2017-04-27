91accb1f41c549f5ceb63cde5a52096424bfdaee
lioliu <744596028@qq.com>
2017-04-27 08:58:12 +0800

    complite the chart of K line

--- 软件/QuantTrade/Client/StockKline.Designer.cs
+++ 软件/QuantTrade/Client/StockKline.Designer.cs
@@ -28,19 +28,306 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title1 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.ChartMain = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.LabName = new System.Windows.Forms.Label();
+            this.LabCode = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
+            this.label17 = new System.Windows.Forms.Label();
+            this.label18 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ChartMain)).BeginInit();
             this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label18);
+            this.groupBox1.Controls.Add(this.label17);
+            this.groupBox1.Controls.Add(this.label16);
+            this.groupBox1.Controls.Add(this.label15);
+            this.groupBox1.Controls.Add(this.label14);
+            this.groupBox1.Controls.Add(this.label13);
+            this.groupBox1.Controls.Add(this.label12);
+            this.groupBox1.Controls.Add(this.label11);
+            this.groupBox1.Controls.Add(this.label10);
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Location = new System.Drawing.Point(12, 81);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(88, 553);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "详细";
+            // 
+            // ChartMain
+            // 
+            chartArea1.Name = "ChartArea1";
+            chartArea1.Position.Auto = false;
+            chartArea1.Position.Height = 75F;
+            chartArea1.Position.Width = 90F;
+            chartArea1.Position.X = 3F;
+            chartArea1.Position.Y = 3F;
+            chartArea2.Name = "ChartArea2";
+            chartArea2.Position.Auto = false;
+            chartArea2.Position.Height = 18.5F;
+            chartArea2.Position.Width = 90F;
+            chartArea2.Position.X = 3F;
+            chartArea2.Position.Y = 81.5F;
+            this.ChartMain.ChartAreas.Add(chartArea1);
+            this.ChartMain.ChartAreas.Add(chartArea2);
+            legend1.Alignment = System.Drawing.StringAlignment.Far;
+            legend1.DockedToChartArea = "ChartArea1";
+            legend1.Docking = System.Windows.Forms.DataVisualization.Charting.Docking.Top;
+            legend1.Name = "Legend1";
+            this.ChartMain.Legends.Add(legend1);
+            this.ChartMain.Location = new System.Drawing.Point(106, 12);
+            this.ChartMain.Name = "ChartMain";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Candlestick;
+            series1.IsXValueIndexed = true;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            series1.YValuesPerPoint = 4;
+            series2.ChartArea = "ChartArea2";
+            series2.Legend = "Legend1";
+            series2.Name = "Series2";
+            this.ChartMain.Series.Add(series1);
+            this.ChartMain.Series.Add(series2);
+            this.ChartMain.Size = new System.Drawing.Size(1196, 652);
+            this.ChartMain.TabIndex = 1;
+            this.ChartMain.Text = "chart1";
+            title1.Name = "Title1";
+            title1.Text = "K线";
+            this.ChartMain.Titles.Add(title1);
+            // 
+            // LabName
+            // 
+            this.LabName.AutoSize = true;
+            this.LabName.Font = new System.Drawing.Font("宋体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.LabName.Location = new System.Drawing.Point(8, 12);
+            this.LabName.Name = "LabName";
+            this.LabName.Size = new System.Drawing.Size(85, 19);
+            this.LabName.TabIndex = 2;
+            this.LabName.Text = "浦东银行";
+            // 
+            // LabCode
+            // 
+            this.LabCode.AutoSize = true;
+            this.LabCode.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.LabCode.Location = new System.Drawing.Point(8, 45);
+            this.LabCode.Name = "LabCode";
+            this.LabCode.Size = new System.Drawing.Size(69, 20);
+            this.LabCode.TabIndex = 3;
+            this.LabCode.Text = "600000";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 30);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 12);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "时间";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 59);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 12);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "时间";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 87);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(29, 12);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "开盘";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(6, 120);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(29, 12);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "开盘";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(6, 143);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(29, 12);
+            this.label7.TabIndex = 4;
+            this.label7.Text = "最高";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(6, 166);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(29, 12);
+            this.label8.TabIndex = 5;
+            this.label8.Text = "最高";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(7, 220);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(35, 12);
+            this.label9.TabIndex = 6;
+            this.label9.Text = "最低 ";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(6, 248);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(35, 12);
+            this.label10.TabIndex = 7;
+            this.label10.Text = "最低 ";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(26, 294);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(29, 12);
+            this.label11.TabIndex = 8;
+            this.label11.Text = "收盘";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(26, 316);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(29, 12);
+            this.label12.TabIndex = 9;
+            this.label12.Text = "收盘";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(24, 358);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(29, 12);
+            this.label13.TabIndex = 10;
+            this.label13.Text = "涨跌";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(24, 419);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(29, 12);
+            this.label14.TabIndex = 11;
+            this.label14.Text = "涨幅";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Location = new System.Drawing.Point(26, 383);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(29, 12);
+            this.label15.TabIndex = 12;
+            this.label15.Text = "涨跌";
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.Location = new System.Drawing.Point(26, 447);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(29, 12);
+            this.label16.TabIndex = 13;
+            this.label16.Text = "涨幅";
+            // 
+            // label17
+            // 
+            this.label17.AutoSize = true;
+            this.label17.Location = new System.Drawing.Point(31, 479);
+            this.label17.Name = "label17";
+            this.label17.Size = new System.Drawing.Size(29, 12);
+            this.label17.TabIndex = 14;
+            this.label17.Text = "总手";
+            // 
+            // label18
+            // 
+            this.label18.AutoSize = true;
+            this.label18.Location = new System.Drawing.Point(31, 505);
+            this.label18.Name = "label18";
+            this.label18.Size = new System.Drawing.Size(29, 12);
+            this.label18.TabIndex = 15;
+            this.label18.Text = "总手";
             // 
             // StockKline
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1080, 716);
+            this.ClientSize = new System.Drawing.Size(1314, 768);
+            this.Controls.Add(this.LabCode);
+            this.Controls.Add(this.LabName);
+            this.Controls.Add(this.ChartMain);
+            this.Controls.Add(this.groupBox1);
             this.Name = "StockKline";
             this.Text = "StockKline";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ChartMain)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label18;
+        private System.Windows.Forms.Label label17;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataVisualization.Charting.Chart ChartMain;
+        private System.Windows.Forms.Label LabCode;
+        private System.Windows.Forms.Label LabName;
     }
 }
--- 软件/QuantTrade/Client/StockKline.cs
+++ 软件/QuantTrade/Client/StockKline.cs
@@ -7,14 +7,64 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using QuantTradeDLL.Crawler;
+using System.Windows.Forms.DataVisualization.Charting;
 namespace Client
 {
     public partial class StockKline : Form
     {
-        public StockKline()
+        private string Code { set; get; }
+        public StockKline(string code)
         {
+
             InitializeComponent();
+            Code = code;
+            
+            StockHisData data = StockHisData.GetHisData(Code, "-25", "-1");
+            LabCode.Text = code;
+            LabName.Text = QuantTradeDLL.DBUtility.OracleClient.GetData($"select name from stock_list where code = '{Code}'").Tables[0].Rows[0][0].ToString().Replace(" ","");
+
+            //foreach (var item in data.Kline)
+            //{
+            //    Console.WriteLine(item.Date);
+            //}
+
+            ChartMain.Series[0].ChartType = SeriesChartType.Candlestick;
+
+            // Set the style of the open-close marks
+            ChartMain.Series[0]["OpenCloseStyle"] = "Triangle";
+
+            // Show both open and close marks
+            ChartMain.Series[0]["ShowOpenClose"] = "Both";
+
+            // Set point width
+            ChartMain.Series[0]["PointWidth"] = "1.0";
+
+            // Set colors bars
+            ChartMain.Series[0]["PriceUpColor"] = "Green"; // <<== use text indexer for series
+            ChartMain.Series[0]["PriceDownColor"] = "Red"; // <<== use text indexer for series
+
+            int i = 0;
+            foreach (var item in data.Kline)
+            {
+                ChartMain.Series[0].Points.AddXY(new DateTime(Convert.ToInt32(item.Date.Substring(0, 4)), Convert.ToInt32(item.Date.Substring(4, 2)), Convert.ToInt32(item.Date.Substring(6, 2))), item.High);
+
+                ChartMain.Series[0].Points[i].YValues[1] =item.Low;
+                //adding open
+                ChartMain.Series[0].Points[i].YValues[2] = item.Open;
+                // adding close
+                ChartMain.Series[0].Points[i].YValues[3] = item.Close;
+        
+                i++;
+
+            }
+            ChartMain.Series[0].XValueType = ChartValueType.DateTime;
+            //Convert.ToDouble(new DateTime(Convert.ToInt32(str.Substring(0, 4)), Convert.ToInt32(str.Substring(4, 2)), Convert.ToInt32(str.Substring(6, 2))))
+
+
         }
+
+
+        
     }
 }
